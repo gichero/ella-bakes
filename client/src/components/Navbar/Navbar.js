5ab5e9d1f13afac9731895b225df726573ecb17d
Amos Gichero <amosgichero@gmail.com>
2020-02-14 23:09:43 -0500

    navbar created

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import TypoGraphy from '@material-ui/core/Typography';
 
 export default function Navbar() {
     return (
-        <div>
-           <h1>Navbar</h1> 
-        </div>
+        <AppBar>
+            <Toolbar>
+            <List component="nav">
+            <ListItem component="div">
+                <ListItemText inset>
+                    <TypoGraphy color="inherit" variant="title">
+                        About
+                    </TypoGraphy>
+                </ListItemText>
+                <ListItemText inset>
+                    <TypoGraphy color="inherit" variant="title">
+                        Menu
+                    </TypoGraphy>
+                </ListItemText>
+                <ListItemText inset>
+                    <TypoGraphy color="inherit" variant="title">
+                        Custom
+                    </TypoGraphy>
+                </ListItemText>
+                <ListItemText inset>
+                    <TypoGraphy color="inherit" variant="title">
+                        Order
+                    </TypoGraphy>
+                </ListItemText>
+            </ListItem >
+            </List>
+            </Toolbar>
+        </AppBar>    
     )
 }
